170c99c244f630268ba9823b69cfe2d277df3aba
wednesday331 <65116875+wednesday331@users.noreply.github.com>
2020-09-30 14:27:50 -0400

    minor fixes

--- import.py
+++ import.py
@@ -12,14 +12,14 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
-#CSV Reader
+# CSV Reader
 def main():
     """Reads the CSV file and uploads it to the booklist table."""
     f = open("books.csv")
     reader = csv.reader(f)
-    for isbn, title, author, year in reader: # loop gives each column a name
+    for isbn, title, author, year in reader:  # loop gives each column a name.
         if "isbn" in isbn:
-            continue #skips the title row to avoid error.
+            continue  # Skips the title row to avoid error.
         print(f"Added {title} by{author}, with isbn {isbn}, published {year}.")
 
         db.execute("""INSERT INTO booklist (isbn, title, author, year)
@@ -30,7 +30,7 @@ def main():
                     "author": author,
                     "year":year
                    }
-                  ) # substitute values from CSV line into SQL command
+                  )  # Substitute values from CSV line into SQL command.
     db.commit() 
 
 if __name__=="__main__":

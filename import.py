d7469c9409f271d7a7ed82f2b63de16bb26c047e
wednesday331 <65116875+wednesday331@users.noreply.github.com>
2020-09-30 14:22:07 -0400

    PEP8 friendly changes

--- import.py
+++ import.py
@@ -1,16 +1,20 @@
-import os, csv
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+"""import module to use in application.py"""
 
+import os,
+import csv
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-
 # Set up database
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
-#------------------------------
+
+
 def main():
-    #Reading the csv file and uploading it to the booklist table
+    """Reads the csv file and uploads it to the booklist table."""
     f = open("books.csv")
     reader = csv.reader(f)
     for isbn, title, author, year in reader: # loop gives each column a name
@@ -18,7 +22,15 @@ def main():
             continue #skips the title row to avoid error.
         print(f"Added {title} by{author}, with isbn {isbn}, published {year}.")
 
-        db.execute("INSERT INTO booklist (isbn, title, author, year) VALUES (:isbn, :title, :author, :year)", {"isbn": isbn, "title": title, "author": author, "year":year}) # substitute values from CSV line into SQL command, as per this dict
+        db.execute("""INSERT INTO booklist (isbn, title, author, year)
+                   VALUES (:isbn, :title, :author, :year)
+                   """,
+                   {"isbn": isbn,
+                    "title": title,
+                    "author": author,
+                    "year":year
+                   }
+                  ) # substitute values from CSV line into SQL command
     db.commit() 
 
 if __name__=="__main__":
